c926a71b856621c7fa55342e7044468a1f533aa8
健康蛀牙 <563567277@qq.com>
2019-10-24 23:48:30 +0800

    添加clipboard-data

--- src/commands/pasteImageFromClipboard.ts
+++ src/commands/pasteImageFromClipboard.ts
@@ -1,10 +1,16 @@
 import * as vscode from 'vscode';
+const clip = require('clipboard-data');
 
 export function pasteImageFromClipboardActivate(context: vscode.ExtensionContext) {
 
     let pasteImageFromClipboardDisposable = vscode.commands.registerCommand('writeCnblog.pasteImageFromClipboard',
         () => {
-
+            try {
+                let st = clip.getImage().toString("base64");
+                console.log(st);
+            } catch (error) {
+                vscode.window.showErrorMessage(error.message);
+            }
         });
 
     context.subscriptions.push(pasteImageFromClipboardDisposable);
