b0e33c31cd943f32365a458a7f3978f001c01e55
健康蛀牙 <563567277@qq.com>
2019-10-26 15:21:01 +0800

    添加剪切板里面的图片内容保存文件到硬盘，并插入图片路径到文章内容里面

--- src/commands/pasteImageFromClipboard.ts
+++ src/commands/pasteImageFromClipboard.ts
@@ -1,4 +1,9 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import { blogWorkspace } from '../blog/blog-workspace';
+import { imageDirName } from '../constants';
+
 const clip = require('clipboard-data');
 
 export function pasteImageFromClipboardActivate(context: vscode.ExtensionContext) {
@@ -6,8 +11,25 @@ export function pasteImageFromClipboardActivate(context: vscode.ExtensionContext
     let pasteImageFromClipboardDisposable = vscode.commands.registerCommand('writeCnblog.pasteImageFromClipboard',
         () => {
             try {
-                let st = clip.getImage().toString("base64");
-                console.log(st);
+                let textEditor = vscode.window.activeTextEditor;
+                if (textEditor) {
+                    let pngData = clip.getImage();
+                    let imageShortPath = path.join(imageDirName, `${Date.now().toString()}.png`);
+                    let imagePath = path.join(blogWorkspace.folderPath, imageShortPath);
+                    let writeStream = fs.createWriteStream(imagePath)
+                        .on('close', function () {
+                            if (textEditor) {
+                                var url = `![](${imageShortPath})`;
+                                textEditor.edit(function editDocument(editParams) {
+                                    if (textEditor) {
+                                        editParams.insert(textEditor.selection.active, url);
+                                    }
+                                });
+                            }
+                        });
+                    writeStream.write(pngData);
+                    writeStream.close();
+                }
             } catch (error) {
                 vscode.window.showErrorMessage(error.message);
             }
